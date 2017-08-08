29e87fbf83bd20a6cc4807b78610852864897aa3
Harmen Janssen <harmen@grrr.nl>
2017-08-08 10:46:46 +0200

    Doin the koan

--- src/koans/08_conditionals.clj
+++ src/koans/08_conditionals.clj
@@ -10,12 +10,12 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
           :a
           :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
           []))
 
   "And in such a situation you may have nothing"
